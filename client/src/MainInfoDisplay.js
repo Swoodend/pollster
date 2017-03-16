cc43b43b8bd044a4a2bd2fc1202a2fa244416e4d
Scott Woodend <Scott Woodend>
2017-03-16 16:16:27 -0400

    adjust icon positioning slightly

--- client/src/MainInfoDisplay.js
+++ client/src/MainInfoDisplay.js
@@ -8,21 +8,21 @@ class MainInfoDisplay extends Component {
           <div className="info-col-contents">
             <h1 className="info-header">Customizable</h1>
             <div className="icon fa fa-cog fa-5x"></div>
-            <p>and here is some description</p>
+            <p>and here is some descriptionand here is some descriptio and here is some description and here is some description and here is some description and here is some description</p>
           </div>
         </div>
         <div className="info-col">
           <div className="info-col-contents">
             <h1 className="info-header">Social</h1>
             <div className="icon fa fa-wechat fa-5x"></div>
-            <p>and here is some description</p>
+            <p>and here is some descriptionand here is some descriptio and here is some description and here is some description and here is some description and here is some description</p>
           </div>
         </div>
         <div className="info-col">
           <div className="info-col-contents">
             <h1 className="info-header">Responsive</h1>
-            <div className="icon fa fa-arrows fa-5x"></div>
-            <p>and here is some description</p>
+            <div className="adjust-left icon fa fa-arrows fa-5x"></div>
+            <p>and here is some descriptionand here is some descriptio and here is some description and here is some description and here is some description and here is some description</p>
           </div>
         </div>
       </div>
