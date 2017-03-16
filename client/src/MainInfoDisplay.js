59faa1c7d7c7fb86086932e773393943e28fc9db
Scott Woodend <Scott Woodend>
2017-03-16 16:05:40 -0400

    create and info display

---
+++ client/src/MainInfoDisplay.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class MainInfoDisplay extends Component {
+  render(){
+    return (
+      <div className="info-section">
+        <div className="info-col">
+          <div className="info-col-contents">
+            <h1 className="info-header">Customizable</h1>
+            <div className="icon fa fa-cog fa-5x"></div>
+            <p>and here is some description</p>
+          </div>
+        </div>
+        <div className="info-col">
+          <div className="info-col-contents">
+            <h1 className="info-header">Social</h1>
+            <div className="icon fa fa-wechat fa-5x"></div>
+            <p>and here is some description</p>
+          </div>
+        </div>
+        <div className="info-col">
+          <div className="info-col-contents">
+            <h1 className="info-header">Responsive</h1>
+            <div className="icon fa fa-arrows fa-5x"></div>
+            <p>and here is some description</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainInfoDisplay;
