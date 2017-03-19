ea7ec35bbe68a2393b5470ecaf05021e0bc9e458
Scott Woodend <woodendscott@gmail.com>
2017-03-19 15:55:58 -0400

    build the login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,8 +3,49 @@ import { Link } from 'react-router-dom';
 
 class Login extends Component {
   render(){
+    let styles = {
+      headerStyle: {
+        "color":"deepskyblue",
+        "margin-left":"35px"
+      },
+
+      inputStyle: {
+        "margin": "0 0 25px 5px",
+        "outline": "none",
+        "border-style": "none",
+        "border-bottom": "1px solid black",
+        "padding-left": "5px"
+      },
+
+      fontStyle: {
+        "font-family": "Asap, Arial"
+      },
+
+      labelStyle: {
+        "display":"inline-block",
+        "width": "100px"
+      }
+    };
+
     return (
-      <h2>You are at the login page</h2>
+      <div className="login-container">
+        <div className="login-box">
+          <h1 style={styles.headerStyle}>Login</h1>
+          <form style={styles.fontStyle} className="login-form">
+            <div>
+              <label style={styles.labelStyle}>Username:</label>
+              <input style={styles.inputStyle} type="text" required="true"/>
+            </div>
+            <div>
+              <label style={styles.labelStyle}>Password:</label>
+              <input style={styles.inputStyle} type="password" required="true"/>
+            </div>
+            <div>
+              <input id="login-submit" type="submit" value="Login"/>
+            </div>
+          </form>
+        </div>
+      </div>
     );
   }
 }
