ce04e12eec0d761bccdb1b4b01049502e02beb96
Scott Woodend <woodendscott@gmail.com>
2017-03-22 12:40:46 -0400

    cleanup unnecessary flash message logic

--- client/src/FlashMessage.js
+++ client/src/FlashMessage.js
@@ -2,14 +2,8 @@ import React, { Component } from 'react';
 
 class FlashMessage extends Component {
   render(){
-    let componentClasses = ["flash-container"];
-
-    if (this.props.fade){
-      componentClasses.push("fade-out");
-    }
-
     return (
-      <div className={componentClasses.join(' ')}>
+      <div className="flash-container">
         <h2>{this.props.type}</h2>
         <p>{this.props.message}</p>
       </div>
