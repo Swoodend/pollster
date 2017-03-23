3dcc72e6653dba8460bcc6c7d4f51e3cf8d7287e
Scott Woodend <Scott Woodend>
2017-03-23 14:20:44 -0400

    if user tries to access /dashboard without being logged in, they are redirected to /login and a flash message is displayed

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -3,16 +3,17 @@ import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
 
-  constructor(props){
-    super(props);
-  }
-
   verifyLoggedIn(){
     return localStorage.getItem("jwt");
   }
 
   render(){
-    let page = this.verifyLoggedIn() ? <h1>Welcome to dashboard</h1> : <Redirect to="/login"/>
+    let page = this.verifyLoggedIn() ?
+      <h1>Welcome to dashboard</h1> :
+      <Redirect
+        to={{pathname: "/login",
+        state: {type: "Warning", message:"Log in before viewing dashboard"}}}
+      />
     return (
       <div>
         {page}
