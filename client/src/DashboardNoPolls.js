9196f0a4d11469c1d366b3101d8f7b4862da8c80
Scott Woodend <Scott Woodend>
2017-03-24 15:19:50 -0400

    create DashboardNoPolls component

---
+++ client/src/DashboardNoPolls.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class DashboardNoPolls extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      currentUser: ''
+    }
+  }
+
+  componentWillMount(){
+    let token = localStorage.getItem('jwt');
+    console.log(token);
+    fetch(`/validate/${token}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((userObj) => {
+        //userObj --- {user: "swood"}
+        this.setState({
+          currentUser: userObj.user
+        });
+      })
+  }
+
+
+  render(){
+    return (
+      <div className="dash-container">
+        <div className="dash-content">
+          <h1 style={{"text-align":"center"}}>{this.state.currentUser}, welcome to your dashboard.
+            You have no polls yet.
+          </h1>
+          <div className="action-container">
+            <div className="new-poll"><Link to="/new">New Poll</Link></div>
+            <div style={{"background": "#C1D42F"}} className="random-poll">
+              <Link to="#">Random</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DashboardNoPolls;
