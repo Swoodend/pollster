10a6131898b70ccd413633ca7f2cdca56914a998
Scott Woodend <woodendscott@gmail.com>
2017-03-27 00:07:10 -0400

    saves curent user in localStorage

--- client/src/Login.js
+++ client/src/Login.js
@@ -57,6 +57,7 @@ class Login extends Component {
         if (res.type === "OK"){
           //set token, clear form fields, send back to / (for now, redirect to dashboard once built)
           localStorage.setItem('jwt', res.token);
+          localStorage.setItem('currentUser', res.currentUser);
           this.resetFormFields();
           window.location.replace('http://localhost:3000/dashboard');
         } else {
