73ce6fde5498706dfeb62639cfcd1bd75957acb6
Scott Woodend <woodendscott@gmail.com>
2017-03-27 00:06:45 -0400

    Dashboard component checks if there is a current user. If there is, renders a different view depending on if the user has polls or not. If there is not a current user, redirects to the login page

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import DashboardNoPolls from './DashboardNoPolls';
+import DashboardWithPolls from './DashboardWithPolls';
 
 class Dashboard extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      loggedIn: false,
+      currentUserPolls: []
+    }
+  }
 
   verifyLoggedIn(){
     return localStorage.getItem("jwt");
   }
 
+  componentWillMount(){
+    let currentUser = localStorage.getItem('currentUser');
+    fetch(`/${currentUser}/polls`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res.status === "OK" && res.polls.length > 0 ){
+          this.setState({
+            loggedIn: true,
+            currentUserPolls: res.polls
+          });
+        }
+      })
+  }
+
   render(){
-    let page = this.verifyLoggedIn() ?
-      <DashboardNoPolls/> :
-      <Redirect
-        to={{pathname: "/login",
-        state: {type: "Warning", message:"Log in before viewing dashboard"}}}
-      />
+    let view;
+
+    if (this.verifyLoggedIn()){
+      if (this.state.currentUserPolls.length > 0){
+        view = <DashboardWithPolls/>
+      } else {
+        view = <DashboardNoPolls/>
+      }
+    } else {
+      view = <Redirect to={{pathname: "/login", state: {type: "Warning", message:"Log in before viewing dashboard"}}}/>;
+    }
+
     return (
       <div>
-        {page}
+        {view}
       </div>
     );
   }
