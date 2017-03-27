c3570e391970896d8092cbce9b831cabef5aa428
Scott Woodend <woodendscott@gmail.com>
2017-03-27 00:10:14 -0400

    creates DashboardWithPolls view. Will be updated later

---
+++ client/src/DashboardWithPolls.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DashboardWithPolls extends Component {
+  render(){
+    return (
+      <h1>Welcome to the dashboard with polls!</h1>
+    );
+  }
+}
+
+export default DashboardWithPolls;
