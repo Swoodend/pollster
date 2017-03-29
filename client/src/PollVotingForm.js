9ed4e8ffcf67c83c84dd47245d53710876d7f605
Scott Woodend <Scott Woodend>
2017-03-29 11:25:28 -0400

    rewrites voting input HTML to accomodate new styling

--- client/src/PollVotingForm.js
+++ client/src/PollVotingForm.js
@@ -25,10 +25,10 @@ class PollVotingForm extends Component{
   render(){
     let options = this.props.options.map((option, i) => {
       return (
-        <div onChange={this.handleChange} key={i}>
-          <label key={i+1}>{option}</label>
-          <input key={i+2} value={i} type="radio" name="option"/>
-        </div>
+        <label onChange={this.handleChange} key={i} htmlFor={option}>
+          <input type="radio" value={i} name="option" id={option}/>
+          <span className="input-span">{option}</span>
+        </label>
       );
     });
 
