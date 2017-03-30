37835b8a8895a988751e118eca11496113b48904
Scott Woodend <Scott Woodend>
2017-03-30 09:35:51 -0400

    stops users from voting multiple times on the same poll. A flash message is displayed if they attempt to vote multiple times

--- client/src/ViewPoll.js
+++ client/src/ViewPoll.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Chart from 'chart.js';
 import PollVotingForm from './PollVotingForm';
+import FlashMessage from './FlashMessage';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 let poll;
+
 class ViewPoll extends Component {
 
   constructor(props){
@@ -11,7 +14,8 @@ class ViewPoll extends Component {
       pollTitle: '',
       pollAuthor: '',
       pollOptions: '',
-      pollVotes: ''
+      pollVotes: '',
+      error: null
     }
   }
 
@@ -70,43 +74,60 @@ class ViewPoll extends Component {
   }
 
   updateChart(index){
-    console.log('you called update chart with index', index);
-    let updatedVotes = this.state.pollVotes.slice();
-    updatedVotes[index] += 1;
-    //save to db
-    let pollId = this.props.match.params.pollId;
-    let requestConfig = {
-      method: "POST",
-      headers: {'Content-type': 'application/json'},
-      body: JSON.stringify({
-        newVoteState: updatedVotes
-      })
-    };
+    let alreadyVoted = localStorage.getItem('alreadyVoted');
+    if (!alreadyVoted){
+      let updatedVotes = this.state.pollVotes.slice();
+      updatedVotes[index] += 1;
+      //save to db
+      let pollId = this.props.match.params.pollId;
+      let requestConfig = {
+        method: "POST",
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+          newVoteState: updatedVotes
+        })
+      };
 
-    console.log('pollid', pollId);
-    fetch(`/polls/update/${pollId}`, requestConfig)
-      .then((res) => {
-        return res.json();
-      })
-      .then ((res) => {
-        if (res.status === "OK"){
-          console.log('res completed successfully');
+      console.log('pollid', pollId);
+      fetch(`/polls/update/${pollId}`, requestConfig)
+        .then((res) => {
+          return res.json();
+        })
+        .then ((res) => {
+          if (res.status === "OK"){
+            console.log('res completed successfully');
+            this.setState({
+              pollVotes: updatedVotes
+            }, () => {
+              console.log('chart state is now', this.state);
+              console.log('poll is', poll);
+              poll.data.datasets[0].data = this.state.pollVotes;
+              poll.update();
+              localStorage.setItem('alreadyVoted', 'true');
+            });
+          } else if (res.status === "no poll found"){
+            console.log('didnt find a poll');
+          }
+        })
+    } else {
+      let errorType = 'Already Voted';
+      let errorMessage = 'Sorry, but you have already voted on this poll. You cannot vote again.';
+      this.setState({
+        error: {type: errorType, message: errorMessage}
+      }, () => {
+        window.setTimeout(() => {
           this.setState({
-            pollVotes: updatedVotes
-          }, () => {
-            console.log('chart state is now', this.state);
-            console.log('poll is', poll);
-            poll.data.datasets[0].data = this.state.pollVotes;
-            poll.update();
-          });
-        } else if (res.status === "no poll found"){
-          console.log('didnt find a poll');
-        }
-      })
-
+            error: null
+          })
+        }, 2000)
+      });
+    }
   }
 
   render(){
+    let error = this.state.error ?
+      <FlashMessage type={this.state.error.type} message={this.state.error.message}/> : null;
+
     let votingOptions = this.state.pollOptions ?
       <PollVotingForm
         updateChart={this.updateChart}
@@ -114,11 +135,12 @@ class ViewPoll extends Component {
       /> : <h1 style={{"position":"absolute", "top": "35px"}}>No poll found</h1>;
     return (
       <div>
-        <h1 style={{"marginTop":"100px", "textAlign":"center"}}>{this.state.pollTitle}</h1>
-        <div className="canvas-container">
-          <canvas id="chart"></canvas>
-        </div>
-        {votingOptions}
+        <ReactCSSTransitionGroup transitionName={"flash"} transitionEnterTimeout={0} transitionLeaveTimeout={1000}>{error}</ReactCSSTransitionGroup>
+          <h1 style={{"marginTop":"100px", "textAlign":"center"}}>{this.state.pollTitle}</h1>
+          <div className="canvas-container">
+            <canvas id="chart"></canvas>
+          </div>
+          {votingOptions}
       </div>
     );
   }
