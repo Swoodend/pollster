66cc1869b0b9420bba6b0a123688fceb3d5a596e
Scott Woodend <Scott Woodend>
2017-03-31 15:04:05 -0400

    DashboardWithPolls component will now render a DashboardPollDisplay component with random poll data to display on the users dashboard

--- client/src/DashboardWithPolls.js
+++ client/src/DashboardWithPolls.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import DashboardPollDisplay from './DashboardPollDisplay';
 
 class DashboardWithPolls extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      pollData: ''
+    };
+  }
+
+  getRandomPoll(arr, max){
+    let index = Math.floor(Math.random() * (arr.length));
+    return arr[index];
+  }
+
+  componentWillMount(){
+    let currentUser = localStorage.getItem("currentUser");
+    fetch(`/${currentUser}/polls`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((user) => {
+        let poll = this.getRandomPoll(user.polls, user.polls.length)
+        console.log('in dwp', poll);
+        this.setState({
+          pollData: poll
+        });
+      });
+  }
   render(){
+    let showPoll = this.state.pollData ? <DashboardPollDisplay pollInfo={this.state.pollData}/> : null;
     return (
-      <h1>Welcome to the dashboard with polls!</h1>
+      <div>
+        {showPoll}
+      </div>
     );
   }
 }
