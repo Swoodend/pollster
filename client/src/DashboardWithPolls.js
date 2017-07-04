7cecebded50d3fb22707ab6aee1a72c18d799c8d
Scott Woodend <woodendscott@gmail.com>
2017-07-04 17:17:52 -0400

    dashboard with polls now renders a random poll that the user created when visited

--- client/src/DashboardWithPolls.js
+++ client/src/DashboardWithPolls.js
@@ -1,9 +1,80 @@
 import React, { Component } from 'react';
+import Chart from 'chart.js';
 
 class DashboardWithPolls extends Component {
+
+  componentWillMount(){
+    this.getRandomPolls();
+  }
+
+  getRandomPolls(){
+    let currentUser = localStorage.getItem('currentUser');
+    let requestConfig = {
+      method: "GET",
+      headers: { "Content-type":"application/json" }
+    }
+    fetch('/'+ currentUser + '/polls', requestConfig)
+      .then((res) => {
+        return res.json();
+      })
+      .then((pollData) => {
+        this.displayPollData(pollData);
+      })
+      .catch((err) => {
+        if(err){
+          console.log('something wrong with your fetch', err);
+        }
+      })
+  }
+
+  displayPollData(pd){
+    let pollData = pd.polls;
+    let randomPoll = pollData[Math.floor(Math.random() * pollData.length)]
+    console.log(randomPoll);
+    console.log(randomPoll.votes)
+    let ctx = document.getElementById('chart').getContext('2d');
+    let poll = new Chart(ctx, {
+      type: 'bar',
+      data : {
+        labels: randomPoll.options,
+        datasets: [{
+          data: randomPoll.votes,
+          backgroundColor: [
+            'deepskyblue',
+            '#FCB723'
+          ],
+          borderColor: [
+            'black',
+            'black'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    })
+  }
+
   render(){
     return (
-      <h1>Welcome to the dashboard with polls!</h1>
+      <div>
+        <h1 style={{textAlign: "center"}}>A random poll you own</h1>
+        <div className="canvas-container">
+          <canvas id="chart"></canvas>
+        </div>
+      </div>
     );
   }
 }
