675cd66ec14829fc80a19eb3bb67b0020f9d2f57
Scott Woodend <Scott Woodend>
2017-07-25 13:29:51 -0400

    renders new navar to be shown when screen size <= 570px

--- client/src/NavbarLoggedIn.js
+++ client/src/NavbarLoggedIn.js
@@ -13,15 +13,31 @@ class NavbarLoggedIn extends Component {
   }
   render(){
     return (
-      <div className="navbar">
-        <div className="left">
-          <div className="nav-title"><Link to="/">Pollster</Link></div>
+      <div>
+        <div className="navbar">
+          <div className="left">
+            <div className="nav-title"><Link to="/">Pollster</Link></div>
+          </div>
+          <div className="right">
+            <div className="username">{this.props.username}</div>
+            <div className="polls"><Link to="/dashboard"><div className="div-anchor">Polls</div></Link></div>
+            <div className="nav-new-poll"><Link to="/new"><div className="div-anchor">New Poll</div></Link></div>
+            <div className="logout"><div onClick={this.handleLogout} className="div-anchor">Logout</div></div>
+          </div>
         </div>
-        <div className="right">
-          <div className="username">{this.props.username}</div>
-          <div className="polls"><Link to="/dashboard"><div className="div-anchor">Polls</div></Link></div>
-          <div className="nav-new-poll"><Link to="/new"><div className="div-anchor">New Poll</div></Link></div>
-          <div className="logout"><div onClick={this.handleLogout} className="div-anchor">Logout</div></div>
+
+        <div className="secondary-nav">
+          <div className="sec-left">
+            <div className="sec-nav-title"><Link to ="/">Pollster</Link></div>
+          </div>
+          <div className="sec-right"> Options
+            <div className="sec-right-menu">
+              <div className="sec-username">{this.props.username}</div>
+              <div className="sec-polls"><Link to="/dashboard"><div className="div-anchor">Polls</div></Link></div>
+              <div className="sec-nav-new-poll"><Link to="/new"><div className="div-anchor">New Poll</div></Link></div>
+              <div className="sec-logout"><div onClick={this.handleLogout} className="div-anchor">Logout</div></div>
+            </div>
+          </div>
         </div>
       </div>
     );
