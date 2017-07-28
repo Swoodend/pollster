69905a22d6b26bc60b8971f9a6aaca03c7bc7703
Scott Woodend <Scott Woodend>
2017-07-28 14:49:24 -0400

    renders the elements that will facilitate the poll deletion process

---
+++ client/src/DeletePollModal.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+//have to style this modal
+export default class DeletePollModal extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleNoClicked = this.handleNoClicked.bind(this);
+    this.handleYesClicked = this.handleYesClicked.bind(this);
+  }
+
+  handleNoClicked(){
+    console.log('you clicked the no button');
+    this.props.removeModal();
+  }
+
+  handleYesClicked(){
+    console.log('you clicked the yes button');
+  }
+
+  render(){
+    return (
+      <div className="modal">
+        <h5>Are you sure you want to delete the poll: {this.props.pollTitle}?</h5>
+        <div className="btn-wrapper">
+          <div onClick={this.handleYesClicked} className="yes-btn">
+            YES
+          </div>
+          <div  onClick={this.handleNoClicked} className="no-btn">
+            NO
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
