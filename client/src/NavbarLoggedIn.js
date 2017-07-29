d92026d4b98a403f3a223211f4556ef040e42956
Scott Woodend <woodendscott@gmail.com>
2017-07-28 22:30:21 -0400

    anchor tag allows for a reload when user clicks "polls" while already on their dashbaord. The page reoload is needed to update state when a user deletes all of their polls. The reload means the app correctly shows the "DashboardNoPolls" component

--- client/src/NavbarLoggedIn.js
+++ client/src/NavbarLoggedIn.js
@@ -20,7 +20,7 @@ class NavbarLoggedIn extends Component {
           </div>
           <div className="right">
             <div className="username">{this.props.username}</div>
-            <div className="polls"><div className="overlay"><Link to="/dashboard"><div className="div-anchor">Polls</div></Link></div></div>
+            <div className="polls"><div className="overlay"><a href="/dashboard"><div className="div-anchor">Polls</div></a></div></div>
             <div className="nav-new-poll"><div className="overlay"><Link to="/new"><div className="div-anchor">New Poll</div></Link></div></div>
             <div className="logout"><div className="overlay"><div onClick={this.handleLogout} className="div-anchor">Logout</div></div></div>
           </div>
