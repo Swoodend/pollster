8b7c68dfdb93d635623eda09882e7c4032d89eac
Scott Woodend <woodendscott@gmail.com>
2017-07-30 19:14:14 -0400

    creates PollStatsArea component

---
+++ client/src/PollStatsArea.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PollStatsArea extends Component {
+  render(){
+    return (
+      <div style={{background:"red", position: "relative", height: "75vh", width:"75vw", display:"flex", float:"right"}}></div>
+    )
+  }
+}
