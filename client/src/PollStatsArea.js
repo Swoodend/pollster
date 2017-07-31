9bfcbab006f4d9a9b5c162959438390e0dfb4de1
Scott Woodend <Scott Woodend>
2017-07-31 15:14:29 -0400

    poll stats area will display most popular poll and total # of votes a user has received across all polls.

--- client/src/PollStatsArea.js
+++ client/src/PollStatsArea.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 
 export default class PollStatsArea extends Component {
   render(){
+    let title = this.props.mostPopularPoll ? this.props.mostPopularPoll.title : null;
+
     return (
       <div className="poll-stats-container">
-        <div className="poll-stats-subcontainer">
-
+        <div id="most-popular-chart" className="poll-stats-subcontainer">
+          <h2>{title}</h2>
         </div>
 
         <div className="poll-stats-subcontainer">
-
+          <h2>Total Votes Across All Polls</h2>
         </div>
 
       </div>
