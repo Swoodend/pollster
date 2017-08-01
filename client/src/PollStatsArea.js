68e99c64247b50f7a9126800d5fd9988b5c0e031
Scott Woodend <woodendscott@gmail.com>
2017-07-31 21:55:08 -0400

    structures PollStatsArea and now displays a chart of the most popular poll

--- client/src/PollStatsArea.js
+++ client/src/PollStatsArea.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
+import Chart from 'chart.js';
 
 export default class PollStatsArea extends Component {
   render(){
+
     let title = this.props.mostPopularPoll ? this.props.mostPopularPoll.title : null;
+    if (this.props.mostPopularPoll){
+      let ctx = document.getElementById('most-popular-chart').getContext('2d');
+      let poll = new Chart(ctx, {
+          type: 'polarArea',
+          data: {
+            labels: this.props.mostPopularPoll.options,
+            datasets: [{
+              data: this.props.mostPopularPoll.votes,
+              backgroundColor: [
+                'deepskyblue',
+                '#FCB723',
+                'orange',
+                'rebeccapurple'
+              ],
+              borderColor: [
+                'black',
+                'black'
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            legend: {
+              display: true
+            },
+            scales: {
+              display: false
+            },
+            scale: {
+              display: false
+            }
+          }
+        });
+    }
+
+
+
 
     return (
       <div className="poll-stats-container">
-        <div id="most-popular-chart" className="poll-stats-subcontainer">
-          <h2>{title}</h2>
+        <div className="poll-stats-subcontainer">
+          <h2>Most Popular Poll</h2>
+          <h3>{title}</h3>
+          <div style={{"height":"350px", "width":"350px", "margin":"0 auto"}}>
+            <canvas style={{"margin":"0 auto"}} id="most-popular-chart"></canvas>
+          </div>
         </div>
 
         <div className="poll-stats-subcontainer">
