2b6cca071c575e7b442dace961deb494eccfa645
Scott Woodend <woodendscott@gmail.com>
2017-08-01 23:16:30 -0400

    this component can now call setModalState in parent component

--- client/src/PollItemNoModal.js
+++ client/src/PollItemNoModal.js
@@ -4,17 +4,28 @@ import DeletePollButton from './DeletePollButton'
 
 
 export default class pollItemNoModal extends Component {
+  constructor(props){
+    super(props);
+
+    this.setModalState = this.setModalState.bind(this);
+  }
+
+  setModalState(){
+    this.props.displayModal();
+  }
+
   render (){
+
     return (
-      <div key={this.props.index} style={{position:"relative"}}>
-        <Link to={`polls/${this.props.id}`}>
-          <div className="poll-item div-anchor">
-            <div className="poll-title">{this.props.title}</div>
-            <div className="poll-votes">Total votes: {this.props.totalVotes}</div>
-          </div>
-        </Link>
-        <DeletePollButton pollId={this.props.id} pollTitle={this.props.title} setModalState={this.props.displayModal}/>
-      </div>
-    )
+        <div className="poll-item div-anchor" style={{"position":"relative"}}>
+          <Link to={`polls/${this.props.id}`}>
+            <div>
+              <div className="pad-left poll-title">{this.props.title}</div>
+              <div className="pad-left poll-votes">Total votes: {this.props.totalVotes}</div>
+            </div>
+          </Link>
+          <DeletePollButton pollId={this.props.id} pollTitle={this.props.title} setModalState={this.props.displayModal}/>
+        </div>
+      )
   }
 }
