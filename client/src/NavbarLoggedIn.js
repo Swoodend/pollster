adf880926c46d5472de411800304244db371bea9
Scott Woodend <woodendscott@gmail.com>
2017-08-05 23:59:44 -0400

    NavbarLoggedIn still renders a regualr or mobile navbar depending on screen width. These were refactored to their own seperate components with state. The state tracks if the nav menu is open or not. This allows the menu to close when a user clicks on a link in the menu instead of staying open as it did previously

--- client/src/NavbarLoggedIn.js
+++ client/src/NavbarLoggedIn.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import NavLoggedInMobile from './NavLoggedInMobile';
+import NavbarLoggedInRegular from './NavbarLoggedInRegular';
 
 class NavbarLoggedIn extends Component {
   constructor(props){
@@ -11,35 +13,16 @@ class NavbarLoggedIn extends Component {
     localStorage.removeItem("currentUser");
     window.location.replace('http://localhost:3000/login');
   }
+
+
+
   render(){
     return (
       <div>
-        <div className="navbar">
-          <div className="left">
-            <div className="nav-title"><Link to="/">Pollster</Link></div>
-          </div>
-          <div className="right">
-            <div className="username">{this.props.username}</div>
-            <div className="polls"><div className="overlay"><Link to="/dashboard"><div className="div-anchor">Polls</div></Link></div></div>
-            <div className="nav-new-poll"><div className="overlay"><Link to="/new"><div className="div-anchor">New Poll</div></Link></div></div>
-            <div className="logout"><div className="overlay"><div onClick={this.handleLogout} className="div-anchor">Logout</div></div></div>
-          </div>
-        </div>
-
-        <div className="secondary-nav">
-          <div className="sec-left">
-            <div className="sec-nav-title"><Link to="/">Pollster</Link></div>
-          </div>
-          <div className="sec-right"> Options <i className="fa fa-caret-down" aria-hidden="true"></i>
-            <div className="sec-right-menu">
-              <div className="sec-username">{this.props.username}</div>
-              <div className="sec-polls"><div className="overlay"><Link to="/dashboard"><div className="div-anchor">Polls</div></Link></div></div>
-              <div className="sec-nav-new-poll"><div className="overlay"><Link to="/new"><div className="div-anchor">New Poll</div></Link></div></div>
-              <div className="sec-logout"><div className="overlay"><div onClick={this.handleLogout} className="div-anchor">Logout</div></div></div>
-            </div>
-          </div>
-        </div>
+        <NavbarLoggedInRegular username={this.props.username} handleLogout={this.handleLogout}/>
+        <NavLoggedInMobile handleLogout={this.handleLogout}/>
       </div>
+
     );
   }
 }
